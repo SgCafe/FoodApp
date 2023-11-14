9b76a63cb89ddc342235e3428b91a12fc26b4d3c
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-11-14 14:12:56 -0300

    feat: add models

---
+++ FoodApp/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodApp.Models
+{
+    [Table("Category")]
+    public class Category
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string NameCategory { get; set; }
+        public string Image { get; set; }
+
+        [OneToMany]
+        public List<Product> Products { get; set; }
+
+    }
+}
---
+++ FoodApp/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodApp.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double? Price { get; set; }
+
+        [ForeignKey(typeof(Category))]
+        public int CategoryId { get; set; }
+
+        [ManyToOne]
+        public Category Category { get; set; }
+    }
+}
